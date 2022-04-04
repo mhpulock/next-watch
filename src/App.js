7f9aa55d22e13afe192c099853a87cba30800e77
mhpulock <ippolok24@gmail.com>
2022-04-04 15:56:57 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
